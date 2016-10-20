0cd43359baa034f1297acf4a6a50cabf57ba48fd
Alex Strutynskyy <kominterna62@gmail.com>
2016-10-20 12:36:30 +0300

    ticket:5 add loader and success message

--- apps/hello/static/js/editprofile.js
+++ apps/hello/static/js/editprofile.js
@@ -18,6 +18,7 @@ function hardcodedUpdate() {
 	$.ajax({
 	url: $(this).attr("href"),
 	cache: false,
+
 	success: function(){
       $('#id_first_name').val(profile.fields[0].first_name);
       $('#id_last_name').val(profile.fields[0].last_name);
@@ -27,11 +28,27 @@ function hardcodedUpdate() {
       $('#id_skype').val(profile.fields[0].skype);
       $('#id_contacts').val(profile.fields[0].contacts);
       $('#id_bio').val(profile.fields[0].bio);
-		},
+
+      $('.loader').css('display', 'none');
+      var message = "<div id='goodmessage' class='col-xs-12" +
+                    " bg-success prof_updated'>" +
+                    "Changes have been save!</div><br><br>";
+      $('.loader').before(message);
+      setTimeout(function() {
+        $('#goodmessage').remove();
+        $('#content-column br').eq(0).remove();
+        $('#content-column br').eq(0).remove();
+        }, 3000);
+		  },
 
   error: function(error){
 		  console.log(error);
-    }
+      }
 	});
+}
+
+function fakeLoader() {
+  $('.loader').css('display', 'block');
+  setTimeout(hardcodedUpdate, 2000);
 }
 
